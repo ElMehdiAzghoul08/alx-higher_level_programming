992271866024f896a79ccba0219ec9949ea7f8bb
ElMehdiAzghoul08 <mehdiharddd899@gmail.com>
2024-01-08 17:11:50 -0800

    no c in my code

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    new_string = ""
+    for character in my_string:
+        if character.lower() != 'c':
+            new_string += character
+        return new_string
