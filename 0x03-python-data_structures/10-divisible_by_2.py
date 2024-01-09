fc651d93c1b3e73a81af07555e8bc88b22e56a76
ElMehdiAzghoul08 <mehdiharddd899@gmail.com>
2024-01-08 18:46:31 -0800

    divisible by 2 and fix 2

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -2,4 +2,8 @@
 def divisible_by_2(my_list=[]):
     if not my_list:
         return []
-        return [number % 2 == 0 for number in my_list]
+        new_list = [number % 2 == 0 for number in my_list]
+        new_list.append(True)
+    else:
+        new_list.append(False)
+    return new_list
