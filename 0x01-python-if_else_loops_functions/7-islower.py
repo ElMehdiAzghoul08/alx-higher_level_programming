7f5bac321be4fe5ae57d74419314566a5aac5ed5
ElMehdiAzghoul08 <mehdiharddd899@gmail.com>
2024-01-10 10:03:15 -0800

    is lower fix2

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def islower(c):
-    for character in range(97, 122):
-        print("{:d} is lower".format(lower))
+    for c in range(97, 122):
+        print("{:d} is lower".format(c))
         return True
     else:
-        print("{:d} is upper".format(character))
+        print("{:d} is upper".format(c))
         return False
