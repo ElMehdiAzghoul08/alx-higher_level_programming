c1d8af4ea0783b88db6c3ec1b29f6f96c989199d
ElMehdiAzghoul08 <mehdiharddd899@gmail.com>
2024-01-10 10:10:21 -0800

    is lower fix 100

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def islower(c):
     if 97 <= ord(c) <= 122:
-        print("{:d} is lower".format(c))
+        print("{:c} is lower".format(c))
         return True
     else:
-        print("{:d} is upper".format(c))
+        print("{:c} is upper".format(c))
         return False
