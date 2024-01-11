17ff3709eb974e9a15906306dbc7d75451d42a43
ElMehdiAzghoul08 <mehdiharddd899@gmail.com>
2024-01-10 17:33:05 -0800

    argument fix

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     if arg == 0:
         print("0 arguments.")
     elif arg == 1:
-        print("1 arguments:")
+        print("1 argument:")
     else:
         print("{} arguments:".format(arg))
     for i in range(arg):
