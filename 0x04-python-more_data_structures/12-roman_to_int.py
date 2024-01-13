f28684dd3d6ca72f9dc8895088a0592dcc5b0597
ElMehdiAzghoul08 <mehdiharddd899@gmail.com>
2024-01-12 20:24:12 -0800

    roman fix 100

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
-def roman_to_int(roman_string):
-    if not isinstance(roman_string, str):
+def roman_to_int(roman_string: str):
+    if roman_string is None or type(roman_string) != str:
         return 0
-    result = 0
-    val = 0
-    roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    for n in reversed(roman_string):
-        val = roman[n]
-        reasult += val if result < val * 5 else -val
-    return result
+    digit = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    num = [digit[a] for a in roman_string] + [0]
+    r = 0
+
+    for n in range(len(num) - 1):
+        if num[n] >= num[n+1]:
+            r += num[n]
+        else:
+            r -= num[n]
+
+            return r
