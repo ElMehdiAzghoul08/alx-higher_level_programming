c3dd839782d5e29b2418c01b44eb98de828d91d9
ElMehdiAzghoul08 <mehdiharddd899@gmail.com>
2024-01-12 20:16:58 -0800

    roman numerals fix

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,11 +2,10 @@
 def roman_to_int(roman_string):
     if not isinstance(roman_string, str):
         return 0
-
-    roman_n = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     result = 0
     val = 0
+    roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     for n in reversed(roman_string):
-        val = roman_n[n]
+        val = roman[n]
         reasult += val if result < val * 5 else -val
     return result
