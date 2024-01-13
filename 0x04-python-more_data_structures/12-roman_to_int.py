fd3cb64837e4d086eb83fdceb4a35eab46efa5fc
ElMehdiAzghoul08 <mehdiharddd899@gmail.com>
2024-01-12 18:19:16 -0800

    Roman to int

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not isinstance(roman_string, str) or roman_string is None:
+        return 0
+
+    roman_n = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+
+    result = 0
+    val = 0
+
+    for n in reversed(roman_string):
+        val = roman_n[n]
+        reasult += val if result < val * 5 else -val
+
+        return result
