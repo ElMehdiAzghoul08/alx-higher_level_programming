580de27b76c016d7352e1f5ae91983925191827b
ElMehdiAzghoul08 <mehdiharddd899@gmail.com>
2024-01-24 17:47:35 -0800

    validation

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python3
+"""
+Represents square class
+"""
+
 
 class Square:
+    """
+    Represents a square
+    """
     def __init__(self, size=0):
+        """
+        init a new square
+        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
