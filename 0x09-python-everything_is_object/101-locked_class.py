20927727a1a6531dd6060c0698883bd3b1fd50dd
ElMehdiAzghoul08 <mehdiharddd899@gmail.com>
2024-02-19 13:30:35 -0800

    4th commit

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 class LockedClass:
-    """class named Locked class"""
+    """
+    class named Locked class
+    """
     def __setattr__(self, name, value):
-        """function setattr"""
+        """
+        function setattr
+        """
         if name != "first_name":
             raise AttributeError("'LockedClass' object has no attribute '{}'".format(name))
         else:
