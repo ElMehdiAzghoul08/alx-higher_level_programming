fb52225f583092b7c4f3eac9f75d99f597bb2738
ElMehdiAzghoul08 <mehdiharddd899@gmail.com>
2024-02-22 10:42:27 -0800

    error fix attempt 3

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-"""Max integer unittest
+"""Module to find the max integer in a list
 """
 
 
 def max_integer(list=[]):
-    """Function
+    """Function to find and return the max integer in a list of integers
+        If the list is empty, the function returns None
     """
     if len(list) == 0:
         return None
