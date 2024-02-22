e55c896fbe43358ad34460b83256a55061279ff2
ElMehdiAzghoul08 <mehdiharddd899@gmail.com>
2024-02-22 10:33:38 -0800

    max_integer push

---
+++ 0x07-python-test_driven_development/tests/6-max_integer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Max integer unittest
+"""
+
+
+def max_integer(list=[]):
+    """Function
+    """
+    if len(list) == 0:
+        return None
+    result = list[0]
+    i = 1
+    while i < len(list):
+        if list[i] > result:
+            result = list[i]
+        i += 1
+    return
