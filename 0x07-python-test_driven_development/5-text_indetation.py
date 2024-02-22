549467c12dd81cd57f016de7f277982c76287ef8
ElMehdiAzghoul08 <mehdiharddd899@gmail.com>
2024-02-22 10:07:37 -0800

    text indentation error fix

--- 0x07-python-test_driven_development/5-text_indetation.py
+++ 0x07-python-test_driven_development/5-text_indetation.py
@@ -13,13 +13,13 @@ def text_indentation(text):
     while c < len(text) and text[c] == ' ':
         c += 1
 
-        while c < len(text):
-            print(text[c], end="")
-            if text[c] == "\n" or text[c] in ".?:":
-                if text[c] in ".?:":
-                    print("\n")
-                    c += 1
-                    while c < len(text) and text[c] == ' ':
-                        c += 1
-                        continue
-                    c += 1
+    while c < len(text):
+        print(text[c], end="")
+        if text[c] == "\n" or text[c] in ".?:":
+            if text[c] in ".?:":
+                print("\n")
+            c += 1
+            while c < len(text) and text[c] == ' ':
+                c += 1
+            continue
+        c += 1
