19e58dd540693268d8edcffbe8c3a75722d35ab5
El Mehdi <azghoulmehdi899@gmail.com>
2024-04-04 00:37:44 +0200

    file convertion

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -6,13 +6,17 @@ import sys
 
 def init_board(n):
     """Initialize an `n`x`n` sized chessboard with 0's."""
-    board = [[" " for _ in range(n)] for _ in range(n)]
-    return board
+    board = []
+    [board.append([]) for i in range(n)]
+    [row.append(' ') for i in range(n) for row in board]
+    return (board)
 
 
 def board_deepcopy(board):
     """Return a deepcopy of a chessboard."""
-    return [row[:] for row in board]
+    if isinstance(board, list):
+        return list(map(board_deepcopy, board))
+    return (board)
 
 
 def get_solution(board):
@@ -22,7 +26,8 @@ def get_solution(board):
         for c in range(len(board)):
             if board[r][c] == "Q":
                 solution.append([r, c])
-    return solution
+                break
+    return (solution)
 
 
 def xout(board, row, col):
@@ -36,31 +41,46 @@ def xout(board, row, col):
         row (int): The row where a queen was last played.
         col (int): The column where a queen was last played.
     """
-    n = len(board)
     # X out all forward spots
-    for c in range(col + 1, n):
+    for c in range(col + 1, len(board)):
         board[row][c] = "x"
     # X out all backwards spots
     for c in range(col - 1, -1, -1):
         board[row][c] = "x"
     # X out all spots below
-    for r in range(row + 1, n):
+    for r in range(row + 1, len(board)):
         board[r][col] = "x"
     # X out all spots above
     for r in range(row - 1, -1, -1):
         board[r][col] = "x"
     # X out all spots diagonally down to the right
-    for r, c in zip(range(row + 1, n), range(col + 1, n)):
+    c = col + 1
+    for r in range(row + 1, len(board)):
+        if c >= len(board):
+            break
         board[r][c] = "x"
+        c += 1
     # X out all spots diagonally up to the left
-    for r, c in zip(range(row - 1, -1, -1), range(col - 1, -1, -1)):
-        board[r][c] = "x"
+    c = col - 1
+    for r in range(row - 1, -1, -1):
+        if c < 0:
+            break
+        board[r][c]
+        c -= 1
     # X out all spots diagonally up to the right
-    for r, c in zip(range(row - 1, -1, -1), range(col + 1, n)):
+    c = col + 1
+    for r in range(row - 1, -1, -1):
+        if c >= len(board):
+            break
         board[r][c] = "x"
+        c += 1
     # X out all spots diagonally down to the left
-    for r, c in zip(range(row + 1, n), range(col - 1, -1, -1)):
+    c = col - 1
+    for r in range(row + 1, len(board)):
+        if c < 0:
+            break
         board[r][c] = "x"
+        c -= 1
 
 
 def recursive_solve(board, row, queens, solutions):
@@ -74,34 +94,33 @@ def recursive_solve(board, row, queens, solutions):
     Returns:
         solutions
     """
-    n = len(board)
-    if queens == n:
+    if queens == len(board):
         solutions.append(get_solution(board))
-        return solutions
+        return (solutions)
 
-    for c in range(n):
+    for c in range(len(board)):
         if board[row][c] == " ":
             tmp_board = board_deepcopy(board)
             tmp_board[row][c] = "Q"
             xout(tmp_board, row, c)
-            solutions = recursive_solve(tmp_board, row + 1, queens + 1, solutions)
+            solutions = recursive_solve(tmp_board, row + 1,
+                                        queens + 1, solutions)
 
-    return solutions
+    return (solutions)
 
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
-    if not sys.argv[1].isdigit():
+    if sys.argv[1].isdigit() is False:
         print("N must be a number")
         sys.exit(1)
-    n = int(sys.argv[1])
-    if n < 4:
+    if int(sys.argv[1]) < 4:
         print("N must be at least 4")
         sys.exit(1)
 
-    board = init_board(n)
+    board = init_board(int(sys.argv[1]))
     solutions = recursive_solve(board, 0, 0, [])
     for sol in solutions:
         print(sol)
