5488321e78e29a6c3f1473653650338e8d0accc6
El Mehdi <azghoulmehdi899@gmail.com>
2024-04-09 05:58:30 +0200

    to integer

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,9 +2,8 @@
 
 const argument = process.argv;
 
-if (Number.isInteger(argument[2])) {
-    console.log(parseInt(argument[2]));
-}
-else {
-    console.log(parseInt(argument[2]));
+if (Number.isInteger(parseInt(argument[2]))) {
+  console.log('My number: ' + parseInt(argument[2]));
+} else {
+  console.log('Not a number');
 }
