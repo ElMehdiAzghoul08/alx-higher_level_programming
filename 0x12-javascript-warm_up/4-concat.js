a3870fc1264101cdfd7e69185aed6821dc8932ba
El Mehdi <azghoulmehdi899@gmail.com>
2024-04-09 06:38:33 +0200

    fix attempt

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -3,7 +3,7 @@
 const argument = process.argv;
 
 if (argument[2] !== undefined && argument[3] === undefined) {
-  console.log(argument[2] + ' ' + 'is' + ' ' + ' ' + argument[3]);
+  console.log(argument[2] + ' ' + 'is' + ' ' + argument[3]);
 } else if (argument[2] === undefined && argument[3] !== undefined) {
   console.log(argument[2] + ' ' + 'is' + ' ' + argument[3]);
 } else {
