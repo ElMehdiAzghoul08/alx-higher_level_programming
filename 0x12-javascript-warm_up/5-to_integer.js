9be6a260aadb002e3cf4810e2d6633da0b4e21ce
El Mehdi <azghoulmehdi899@gmail.com>
2024-04-09 03:52:19 +0200

    fix

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const argument = process.argv;
+
+if (Number.isInteger(argument[2])) {
+    console.log(parseInt(argument[2]));
+}
+else {
+    console.log(parseInt(argument[2]));
+}
