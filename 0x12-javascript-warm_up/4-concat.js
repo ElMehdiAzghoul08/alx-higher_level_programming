f9b1a0ee5ec2533d1437de3045d22924f33f8bba
El Mehdi <azghoulmehdi899@gmail.com>
2024-04-09 05:26:00 +0200

    fix task4

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -4,7 +4,7 @@ const argument = process.argv;
 
 if (argument[2] !== undefined && argument[3] === undefined) {
   console.log(argument[2] + ' ' + 'is' + ' ' + ' ' + argument[3]);
-} else if (argument[2] === undefined && argument[2] !== undefined) {
+} else if (argument[2] === undefined && argument[3] !== undefined) {
   console.log(argument[2] + ' ' + 'is' + ' ' + argument[3]);
 } else {
   console.log(argument[2] + ' ' + 'is' + ' ' + argument[3]);
