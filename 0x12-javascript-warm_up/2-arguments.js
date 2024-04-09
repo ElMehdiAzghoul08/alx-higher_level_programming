3b4ca766fa30da4cb5f1d9ce96bfca2b88cc00e2
El Mehdi <azghoulmehdi899@gmail.com>
2024-04-09 02:30:00 +0200

    Argumnts fix2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,10 +2,10 @@
 
 const argument = process.argv;
 
-if (argument.length === 3) {
-  console.log('Argument found');
-} else if (argument.length > 3) {
-  console.log('Arguments found');
-} else {
+if (argument.length === 2) {
   console.log('No argument');
+} else if (argument.length === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
