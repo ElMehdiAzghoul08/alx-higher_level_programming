ce896d1e3f5c25ea77dcc733c290fac3f4c980c8
El Mehdi <azghoulmehdi899@gmail.com>
2024-04-15 12:03:24 +0200

    fix attempt

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
 exports.esrever = function (list) {
-    const reversed_ = [];
+  const reversed_ = [];
 
-    for (let idx = list.length -1; idx >= 0; idx--) {
-        reversed_.push(list[idx]);
-    }
+  for (let idx = list.length - 1; idx >= 0; idx--) {
+    reversed_.push(list[idx]);
+  }
 
-    return reversed_;
+  return reversed_;
 };
