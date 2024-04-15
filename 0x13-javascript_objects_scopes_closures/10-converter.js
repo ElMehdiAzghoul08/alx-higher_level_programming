48d60dc90b5f6616ed679420839feb22689d8f99
El Mehdi <azghoulmehdi899@gmail.com>
2024-04-15 12:04:45 +0200

    fix attempt2

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
-exports.converter = function (base){
-    return function (result_){
-        return result_.toString(base);
-    };
+exports.converter = function (base) {
+  return function (result_) {
+    return result_.toString(base);
+  };
 };
