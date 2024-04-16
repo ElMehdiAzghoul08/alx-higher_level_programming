50d04bb5de07935d6af45abec035b5df4a328a1f
El Mehdi <azghoulmehdi899@gmail.com>
2024-04-16 13:18:33 +0200

    concat.ja fix

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -6,9 +6,9 @@ const fileA = process.argv[2];
 const fileB = process.argv[3];
 const fileC = process.argv[4];
 
-const contentA_ = fs.readFileSync(fileA, 'utf8');
-const contentB_ = fs.readFileSync(fileB, 'utf8');
+const contentA_ = fs.existsSync(fileA) ? fs.readFileSync(fileA, 'utf8').trim() : '';
+const contentB_ = fs.existsSync(fileA) ? fs.readFileSync(fileB, 'utf8').trim() : '';
 
-const concatContent_ = contentA_.trim() + '\n' + contentB_.trim() + '\n';
+const concatContent_ = contentA_ + (contentA_ && contentB_ ? '\n' : '') + contentB_;
 
 fs.writeFileSync(fileC, concatContent_);
