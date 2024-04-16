01304262ca1c896fffe67fa02ac6854a3ce5a4b2
El Mehdi <azghoulmehdi899@gmail.com>
2024-04-16 12:54:53 +0200

    fix

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,11 +2,12 @@
 
 class Rectangle {
   constructor (w, h) {
-    this.width = w;
-    this.height = h;
     if (w <= 0 || h <= 0) {
-      const emptyObj_ = {};
-      console.log(emptyObj_);
+      this.width = undefined;
+      this.height = undefined;
+    } else {
+      this.width = w;
+      this.height = h;
     }
   }
 }
