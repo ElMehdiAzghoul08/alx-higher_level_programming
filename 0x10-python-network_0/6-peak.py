72996cd955ea685f12731b5aadeb5fae2290c8b4
El Mehdi <azghoulmehdi899@gmail.com>
2024-04-28 19:35:56 +0100

    in progress

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""function that finds a peak in a list of unsorted integers"""
+
+def find_peak(list_of_integers):
+    """function finc_peak"""
+    if not list_of_integers:
+        return None
+    lt = 0
+    rt = len(list_of_integers) - 1
+
+    while lt < rt :
+        ctr = (lt + rt) // 2
+
+        if list_of_integers[ctr] > list_of_integers[ctr + 1]:
+            rt = ctr
+        else:
+            lt = ctr + 1
+
+    return list_of_integers[lt]
