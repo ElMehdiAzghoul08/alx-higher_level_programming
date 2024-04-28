192392065e2a7bb69a615adfd4929d7b13404581
El Mehdi <azghoulmehdi899@gmail.com>
2024-04-28 19:04:40 +0100

    requirements fix

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 #Bash script that takes in a URL, sends a GET request to the URL, displays body
-
 curl -sL "$1"
--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 #sends a DELETE request to the URL passed as the first argument and displays body
-
 curl -sX DELETE "$1"
