cc07402519493aa37a957aafa47895c3dadbd04c
ElMehdiAzghoul08 <mehdiharddd899@gmail.com>
2024-04-29 20:37:26 -0700

    pushing fix

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -11,20 +11,25 @@ if __name__ == "__main__":
     db_name = sys.argv[3]
     name_state = sys.argv[4]
 
-    db = MySQLdb.connect(host="localhost",
-                         port=3306, user=username, passwd=password, db=db_name)
-    cr_ = db.cursor()
-
     try:
+        db = MySQLdb.connect(host="localhost",
+                             port=3306, user=username, passwd=password,
+                             db=db_name)
+
+        cr_ = db.cursor()
+
         qr_ = "SELECT ctz_.name FROM ctz_ JOIN states \
             ON ctz_.state_id = states.id WHERE states.name = %s ORDER BY \
                 ctz_.id ASC"
         cr_.execute(qr_, (name_state,))
         output = cr_.fetchall()
-        ct_ = ', '.join(cty_[0] for cty_ in output)
-        print(ct_)
+        if output:
+            ct_ = ', '.join(cty_[0] for cty_ in output)
+            print(ct_)
+        else:
+            print("no cities found")
+
+        db.close()
 
     except Exception as y:
-        print(y)
-
-    db.close()
+        print(f"MySQL Error [{y.args[0]}]: {y.args[1]}")
