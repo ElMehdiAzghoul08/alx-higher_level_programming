928f436dcd2516f69d87f67de8948b5985ea5758
ElMehdiAzghoul08 <mehdiharddd899@gmail.com>
2024-04-29 20:57:24 -0700

    task 14 fix

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -12,18 +12,13 @@ if __name__ == "__main__":
     password = sys.argv[2]
     db_name = sys.argv[3]
 
-    try:
+    eg_ = create_engine(f'mysql+mysqldb://{username}:\
+                        {password}@localhost:3306/{db_name}')
+    Ssn_ = sessionmaker(bind=eg_)
+    ssn_ = Ssn_()
 
-        eg_ = create_engine(f'mysql+mysqldb://{username}:\
-                            {password}@localhost:3306/{db_name}')
-        Ssn_ = sessionmaker(bind=eg_)
-        ssn_ = Ssn_()
+    ctz_ = ssn_.query(City).order_by(City.id).all()
+    for x in ctz_:
+        print("{}: ({}) {}".format(x.state.name, x.id, x.name))
 
-        ctz_ = ssn_.query(City).order_by(City.id).all()
-        for x in ctz_:
-            print("{}: ({}) {}".format(x.state.name, x.id, x.name))
-
-        ssn_.close()
-
-    except Exception as y:
-        print(f"Error: {y}")
+    ssn_.close()
