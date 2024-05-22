f140ef32918bdb951b26be1d327c7c7783ad8548
El Mehdi <azghoulmehdi899@gmail.com>
2024-05-22 17:10:53 +0100

    task2 fix

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -2,7 +2,7 @@
 const req_ = require('request');
 
 const idMov_ = process.argv[2];
-const url_ = `https://swapi-api.alx-tools.com/api/film_s/${idMov_}`;
+const url_ = `https://swapi-api.alx-tools.com/api/films/${idMov_}`;
 
 req_(url_, (erro_, res_, body) => {
   if (erro_) {
