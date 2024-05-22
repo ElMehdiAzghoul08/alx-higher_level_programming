8307911a2f8d4b87ba40f0c8f207c8c29859872b
El Mehdi <azghoulmehdi899@gmail.com>
2024-05-22 17:31:38 +0100

    task6

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+const req_ = require('request');
+
+const urlApI_ = process.argv[2];
+
+req_(urlApI_, (error_, response_, body_) => {
+  if (error_) {
+    console.error(error_);
+    return;
+  }
+
+  if (response_.statusCode === 200) {
+    const todos_ = JSON.parse(body_);
+    const finisheDTasks_ = todos_.reduce((a_, todo_) => {
+      if (todo_.completed) {
+        a_[todo_.userId] = (a_[todo_.userId] || 0) + 1;
+      }
+      return a_;
+    }, {});
+    console.log(finisheDTasks_);
+  }
+});
